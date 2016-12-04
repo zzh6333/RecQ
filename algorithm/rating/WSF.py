3776628cb935d557599ac7a974e076b4dfc2b229
Coder-Yu <yuyu2223@163.com>
2016-12-04 22:06:02 +0800

    fix bug of measure.py

---
+++ algorithm/rating/WSF.py
@@ -0,0 +1,130 @@
+from baseclass.SocialRecommender import SocialRecommender
+from data import rating
+from tool import config
+import numpy as np
+import networkx as nx
+import pickle
+
+
+
+
+class WSF(SocialRecommender):
+    def __init__(self,conf):
+        super(WSF, self).__init__(conf)
+        self.config = conf
+
+    def readConfiguration(self):
+        super(SocialRecommender, self).readConfiguration()
+        alpha = config.LineConfig(self.config['WSF'])
+        self.alpha = float(alpha['-alpha'])
+
+    def printAlgorConfig(self):
+        super(WSF, self).printAlgorConfig()
+        print 'Specified Arguments of', self.config['recommender'] + ':'
+        print 'alpha: %.3f' % self.alpha
+        print '=' * 80
+
+    def initModel(self):
+        super(WSF, self).initModel()
+        #construct graph
+        G = nx.DiGraph()
+        for re in self.sao.relation:
+            G.add_edge(re[0],re[1])
+        #initialize new relation matrix
+        self.S = self.sao.followees.copy()
+        for u1 in self.S:
+            for u2 in self.S[u1]:
+                self.S[u1][u2] = np.random.rand()
+        #compute betweenness
+        self.getBetweenCentrality(G)
+
+    def getBetweenCentrality(self,G,load=True):
+        self.communication = np.zeros(self.dao.trainingSize()[0])
+        bt = nx.betweenness_centrality(G)
+        if not load:
+            output = open('between.pkl', 'wb')
+            pickle.dump(bt, output)
+        else:
+            pkl_file = open('between.pkl', 'rb')
+            bt = pickle.load(pkl_file)
+        max = np.max(bt.values())
+        min = np.min(bt.values())
+        diff = max - min
+        for u in bt:
+            uid = self.dao.getUserId(u)
+            self.communication[uid] = bt[u]-min/diff
+
+    def buildModel(self):
+        iteration = 0
+        while iteration < self.maxIter:
+            self.loss = 0
+            for triple in self.dao.trainingData:
+                u,i,r = triple
+                trustRating = 0
+                suv = 0
+                if len(self.sao.getFollowees(u)) != 0:
+                    suv = sum(self.S[u].values())  #
+                    for v in self.sao.getFollowees(u):
+                        trustRating += self.S[u][v] * self.dao.rating(v,i)
+
+                u = self.dao.getUserId(u)
+                i = self.dao.getItemId(i)
+                error = r - self.alpha*self.P[u].dot(self.Q[i])-(1-self.alpha)*(trustRating/suv)
+                self.loss += error**2
+                p = self.P[u].copy()
+                q = self.Q[i].copy()
+                s = self.S[u]
+                self.loss += self.regU * p.dot(p) + self.regI * q.dot(q)
+
+                #update latent vectors
+                self.P[u] += self.lRate*(self.alpha*error*q-self.regU*p)
+                self.Q[i] += self.lRate*(self.alpha*error*p-self.regI*q)
+                if suv != 0:
+                    self.S[u] += self.lRate*(1-self.alpha)*error*((r*suv - trustRating)/(suv**2))
+
+            iteration += 1
+            if self.isConverged(iteration):
+                break
+
+            #######
+            for u1 in self.S:
+                for u2 in self.S[u1]:
+                    id = self.dao.getUserId(u2)
+                    self.S[u1][u2]*=self.communication[id]
+
+            self.sao.followees = self.S
+
+
+
+    def predict(self,u,i):
+        # if self.dao.containsUser(u) and self.dao.containsItem(i):
+        #     u = self.dao.getUserId(u)
+        #     i = self.dao.getItemId(i)
+        #
+        #     if len(self.sao.getFollowees(u)) != 0:
+        #         self.cTrustRating = 0
+        #         for v in self.sao.getFollowees(u):
+        #             self.cTrustRating += self.S[u][v] * self.communication[u][v]*self.dao.rating(v, i)
+        #     else:
+        #         pass
+        #     return self.alpha*self.P[u].dot(self.Q[i])+(1-self.alpha)*(self.cTrustRating/suv)
+        # else:
+        #     return self.dao.globalMean
+        if self.dao.containsUser(u) and self.dao.containsItem(i):
+            i = self.dao.getItemId(i)
+            fPred = 0
+            denom = 0
+            relations = self.sao.getFollowees(u)
+            for followee in relations:
+                weight = relations[followee]
+                uf = self.dao.getUserId(followee)
+                if uf <> -1 and self.dao.containsUser(uf):  # followee is in rating set
+                    fPred += weight * (self.P[uf].dot(self.Q[i]))
+                    denom += weight
+            u = self.dao.getUserId(u)
+            if denom <> 0:
+                return self.alpha * self.P[u].dot(self.Q[i])+(1-self.alpha)*fPred / denom
+            else:
+                return self.P[u].dot(self.Q[i])
+        else:
+            return self.dao.globalMean
